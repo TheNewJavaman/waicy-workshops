4e33778df7b6c1cd5d9a7e145fdc0296617c6d19
Gabriel Pizarro <gpizarro@javaman.net>
2021-10-06 13:56:58 +0000

    Partial linreg visualization

--- workshop-1/1-2-car-mpg-analysis.py
+++ workshop-1/1-2-car-mpg-analysis.py
@@ -8,10 +8,10 @@
 #        seaborn: graphs (extension of matplotlib)
 #        sklearn: data processing
 import pandas as pd
-import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+import numpy as np
 
 # 2. Read car data from a .csv file as a dataframe, then print out the first 10 entries;
 #    CSV files can be used in other programs like Excel or Google Sheets
@@ -20,10 +20,12 @@ print(df.head(10))
 
 # 3. Visualize the cars' model years and origins
 sns.set_theme(style="whitegrid")
-fig = sns.countplot(x=df["model year"])
-fig.get_figure().savefig("1-2-car-model-years.png")
-fig = sns.countplot(x=df["origin"])
-fig.get_figure().savefig("1-2-car-origins.png")
+plot = sns.countplot(x=df["model year"]).get_figure()
+plot.savefig("1-2-car-model-years.png")
+plot.clf()
+plot = sns.countplot(x=df["origin"]).get_figure()
+plot.savefig("1-2-car-origins.png")
+plot.clf()
 
 # 4. Create training data to predict any car's mpg;
 #    70% of the data will be used to train the model, and
@@ -38,6 +40,11 @@ X = df.filter([
 y = df["mpg"].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30)
 
-# 5. Train the model using the data
+# 5. Train a model using the data, then visualize horsepower correlation
 model = LinearRegression().fit(X, y)
 y_pred = model.predict(X_test)
+print(np.transpose([X_train[:,2], y_train]).shape)
+train_df = pd.DataFrame(data=np.transpose([X_train[:,2], y_train]), columns=["horsepower", "mpg"])
+fig = sns.regplot(data=train_df).get_figure()
+fig.savefig("1-2-model-correlation.png")
+fig.clf()
