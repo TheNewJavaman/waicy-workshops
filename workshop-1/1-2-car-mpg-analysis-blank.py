9c3c96d6ffa906013f58f76929b3c436d0453b66
Gabriel Pizarro <gpizarro@javaman.net>
2021-10-09 18:52:56 +0000

    Added blank mpg analysis

--- workshop-1/1-2-car-mpg-analysis-blank.py
+++ workshop-1/1-2-car-mpg-analysis-blank.py
@@ -8,25 +8,25 @@
 #        seaborn: graphs (extension of matplotlib)
 #        sklearn: data processing
 #        numpy: data structures
-import pandas as pd
-import seaborn as sns
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-import numpy as np
+import ___ as pd
+import ___ as sns
+from ___.model_selection import train_test_split
+from ___.linear_model import LinearRegression
+import ___ as np
 
 # 2. Read car data from a .csv file as a dataframe, then print out the first 10 entries;
 #    CSV files can be used in other programs like Excel or Google Sheets
-df = pd.read_csv("1-2-car-mpg-data.csv")
-print(df.head(10))
+df = ___.read_csv("1-2-car-mpg-data.csv")
+print(___.head(10))
 
 # 3. Visualize the cars' model years and origins
-sns.set_theme(style="whitegrid")
-fig = sns.countplot(x=df["model year"]).get_figure()
-fig.savefig("1-2-car-model-years.png")
-fig.clf()
-fig = sns.countplot(x=df["origin"]).get_figure()
-fig.savefig("1-2-car-origins.png")
-fig.clf()
+___.set_theme(style="whitegrid")
+plot = ___.countplot(x=df["model year"]).get_figure()
+___.savefig("1-2-car-model-years.png")
+___.clf()
+___ = sns.countplot(x=df["origin"]).get_figure()
+___.savefig("1-2-car-origins.png")
+___.clf()
 
 # 4. Create training data to predict any car's mpg;
 #    70% of the data will be used to train the model, and
@@ -34,32 +34,32 @@ fig.clf()
 #        X: input data (cylinders, displacement, horsepower,
 #                       weight, acceleration, model year, origin)
 #        y: output data (mpg)
-X = df.filter([
+___ = df.filter([
     "cylinders", "displacement", "horsepower", 
     "weight", "acceleration", "model year", "origin"
 ]).values
-y = df["mpg"].values
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30)
+___ = df["mpg"].values
+X_train, X_test, y_train, y_test = train_test_split(___, ___, test_size=0.30)
 
 # 5. Train a model using the data
-model = LinearRegression().fit(X_train, y_train)
+___ = LinearRegression().fit(X_train, y_train)
 y_pred = model.predict(X_test)
 
 # 6. Display prediction accuracy for the test dataset
 accuracies = []
 for i in range(len(y_test)):
-    accuracies.append(100 * abs(y_pred[i] - y_test[i]) / y_test[i])
-accuracies_df = pd.DataFrame({"Index": range(len(y_test)), "% Error": accuracies})
-accuracies_df = accuracies_df.sort_values(["% Error"]).reset_index(drop=True)
-fig = sns.barplot(accuracies_df.index, accuracies_df["% Error"])
-fig.set(xticklabels=[])
-fig = fig.get_figure()
-fig.savefig("1-2-model-error.png")
-fig.clf()
+    ___.append(100 * abs(y_pred[i] - y_test[i]) / y_test[i])
+___ = pd.DataFrame({"Index": range(len(y_test)), "% Error": accuracies})
+___ = accuracies_df.sort_values(["% Error"]).reset_index(drop=True)
+plot = sns.barplot(accuracies_df.index, accuracies_df["% Error"])
+___.set(xticklabels=[])
+___ = fig.get_figure()
+___.savefig("1-2-model-error.png")
+___.clf()
 
 # 7. Visualize horsepower vs. mpg
 #    What's the correlation between a car's horsepower and its mpg?
 horsepower_df = pd.DataFrame({"Horsepower": X_train[:, 2], "MPG": y_train})
-fig = sns.regplot(data=horsepower_df, x="Horsepower", y="MPG").get_figure()
-fig.savefig("1-2-horsepower-correlation.png")
-fig.clf()
+plot = sns.regplot(data=horsepower_df, x="Horsepower", y="MPG").get_figure()
+___.savefig("1-2-horsepower-correlation.png")
+___.clf()
